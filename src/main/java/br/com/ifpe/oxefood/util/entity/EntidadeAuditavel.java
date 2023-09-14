4ce7277acad508950a02c6b40f3907d755b33a93
aluno <aluno@ifpe>
2023-09-14 15:01:29 -0300

    aula 04

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeAuditavel.java
@@ -1,22 +1,42 @@
 package br.com.ifpe.oxefood.util.entity;
+
 import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@MappedSuperclass
+public abstract class EntidadeAuditavel extends EntidadeNegocio {
+  
+   @JsonIgnore
+   @Version
+   private Long versao;
 
-public abstract class EntidadeAuditavel {
+   @JsonIgnore
+   @CreatedDate
+   private LocalDate dataCriacao;
 
-    private Long versao;
+   @JsonIgnore
+   @LastModifiedDate
+   private LocalDate dataUltimaModificacao;
 
-    private LocalDate dataCriacao;
+   @JsonIgnore
+   @Column
+   private Long criadoPor; // Id do usuário que o criou
 
-    private LocalDate dataUltimaModificacao;
-
-    private Long criadoPor; // Id do usuário que o criou
-
-    private Long ultimaModificacaoPor; // Id do usuário que fez a última alteração
+   @JsonIgnore
+   @Column
+   private Long ultimaModificacaoPor; // Id do usuário que fez a última alteração
 
 }

596bfe84631fa60a5df399ccd7fa1ecb45a51565
aluno <aluno@ifpe>
2023-09-29 15:34:40 -0300

    implementacao produto

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -0,0 +1,22 @@
+package br.com.ifpe.oxefood.modelo.produto;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+   @Autowired
+   private ProdutoRepository repository;
+
+   @Transactional
+   public Produto save(Produto produto) {
+
+       produto.setHabilitado(Boolean.TRUE);
+       produto.setVersao(1L);
+       return repository.save(produto);
+   }
+
+}
